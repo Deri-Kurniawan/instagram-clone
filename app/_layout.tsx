d9201702097ca039a6f21aac1051e82c67355a5d
Deri Kurniawan <deri.netuchi@gmail.com>
2023-03-12 21:03:03 +0700

    feat: add main screen

---
+++ app/_layout.tsx
@@ -0,0 +1,47 @@
+import { SplashScreen, Stack, Slot, Link } from "expo-router";
+import { View, Image, TouchableWithoutFeedback } from "react-native";
+import { Tabs } from "expo-router";
+import { useFonts } from "expo-font";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+import { GlobalProvider } from "../providers/GlobalProvider";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Billabong: require("../assets/fonts/FontsFree-Net-Billabong.ttf"),
+    ProximaNova: require("../assets/fonts/FontsFree-Net-proxima_nova_reg-webfont.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{
+        headerTitle: "Instagram",
+        headerTitleStyle: {
+          fontFamily: "Billabong",
+          fontSize: 40,
+        },
+        headerShadowVisible: false,
+        headerShown: !false,
+        headerRight: () => {
+          return (
+            <View style={{ flexDirection: "row", justifyContent: "flex-end", alignItems: "center" }}>
+              <Link href="/notifications" style={{ paddingLeft: 8, paddingVertical: 9, paddingRight: 8 }}>
+                <Feather name="heart" size={29} color="black" />
+              </Link>
+
+              <Link href="/messages" style={{ paddingLeft: 8, paddingVertical: 8, paddingRight: 2 }}>
+                <MaterialCommunityIcons name="facebook-messenger" size={32} color="black" />
+              </Link>
+            </View>
+          );
+        }
+      }}
+      />
+    </GlobalProvider>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,93 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image, ScrollView, RefreshControl } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import StoryBubble from "../components/StoryBubble";
+import { useGlobal } from "../providers/GlobalProvider";
+import FeedCard from "../components/Feed/FeedCard";
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
-}
+  const { user, users, posts } = useGlobal();
+  const [sortUsersByStoryNotSeen, setSortUsersByStoryNotSeen] = useState(() => {
+    return users.sort((a, b) => {
+      if (a.storyIsSeen === b.storyIsSeen) {
+        return 0;
+      }
+      if (a.storyIsSeen) {
+        return 1;
+      }
+      return -1;
+    });
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setSortUsersByStoryNotSeen((prev) => {
+      return prev.sort((a, b) => {
+        if (a.storyIsSeen === b.storyIsSeen) {
+          return 0;
+        }
+        if (a.storyIsSeen) {
+          return 1;
+        }
+        return -1;
+      });
+    })
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <StatusBar />
+      <View style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#fff",
+      }}>
+
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor="#fff"
+              titleColor="#fff"
+              progressBackgroundColor="#fff"
+              title="Refreshing..."
+              progressViewOffset={50}
+            />
+          }>
+          <View style={{
+            flexDirection: "row", borderBottomColor: "#DADBDA",
+            borderBottomWidth: 1,
+          }}>
+            {/* users stories */}
+            <FlatList
+              keyExtractor={(item, index) => index.toString()}
+              style={{ backgroundColor: "white", paddingVertical: 8 }}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              data={sortUsersByStoryNotSeen.filter((item) => item.id !== user.id)}
+              ListHeaderComponent={() => <StoryBubble {...user} username="Your Story" />}
+              renderItem={({ item }) => <StoryBubble {...item} />}
+            />
+          </View>
+          <View>
+            <FlatList
+              keyExtractor={(item, index) => index.toString()}
+              horizontal={false}
+              showsHorizontalScrollIndicator={false}
+              data={posts}
+              renderItem={({ item }) => <FeedCard {...item} />}
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+    </>
+  )
+}
